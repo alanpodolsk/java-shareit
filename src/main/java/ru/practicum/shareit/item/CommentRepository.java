a23eb07b37b3ee31005a2a05081bf0e8e23774d5
alanpodolsk <alanpodolsk@yandex.ru>
2023-09-17 10:05:57 +0300

    Fixed CommentRepository.findAllByItemId

--- src/main/java/ru/practicum/shareit/item/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/CommentRepository.java
@@ -1,10 +1,12 @@
 package ru.practicum.shareit.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.item.model.Comment;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Integer> {
+    @Query(value = "select * from comments where item_id = ?1", nativeQuery = true)
     List<Comment> findAllByItemId(Integer itemId);
 }
